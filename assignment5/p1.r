0c0679b9d2cc705aa731228d52ae195dddc2bf9f
Feier Lian <flian2@ncsu.edu>
2015-06-23 07:40:22 +0800

    submitted

---
+++ assignment5/p1.r
@@ -0,0 +1,31 @@
+raw_data <- read.csv(file = "seaflow_21min.csv", header = TRUE, sep = ",");
+library(caret);
+set.seed(1234);
+trainIndex <- createDataPartition(raw_data$pop, p = .5,
+                                  list = FALSE,
+                                  times = 1);
+trainPart <- raw_data[trainIndex,];
+testPart <- raw_data[-trainIndex,];
+# plot pe vs chl_small
+library(ggplot2)
+qplot( raw_data$chl_small, raw_data$pe, color = raw_data$pop);
+library(rpart)
+fol <- formula(pop ~ fsc_small + fsc_perp + fsc_big + pe + chl_big + chl_small)
+model <- rpart(fol, method="class", data = trainPart);
+predicted_label <- predict(model, testPart, type = "class");
+accuracy = sum(predicted_label == testPart$pop)/dim(testPart)[1];
+
+# build random forest
+library(randomforest)
+model <- randomForest(fol, data=trainPart);
+predicted_forest = predict(model, testPart, type = "class");
+accuracy_forest = sum(predicted_forest == testPart$pop)/dim(testPart)[1];
+
+# SVM
+library(e1071)
+model3 <- svm(fol, data=trainPart);
+predicted_svm = predict(model, testPart, type = "class");
+accuracy_svm = sum(predicted_svm == testPart$pop)/dim(testPart)[1];
+
+# construct confusion matrix
+table(pred = predicted_svm, true = testingdata$pop)
