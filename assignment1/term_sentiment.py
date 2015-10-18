e23a636435a138981b90fb9dd1315ea457ee6cbd
Feier Lian <flian2@ncsu.edu>
2015-10-18 17:41:15 -0400

    derive term semtiment

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,13 +1,20 @@
+# derived score is decided by the neareast 2 word on the left and right
+# that appears in the dictionary.
 
+# Build a dictionary new_scores to store the derived (term, score)
+# iterate each line of tweets to cummulate scores
 
 import sys
 import json
 
-def hw():
-    print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
+
+def uni2utf8(uni_string):
+    unicode_string = uni_string
+    encoded_string = unicode_string.encode('utf-8')
+    return encoded_string
 
 def main():
     sent_file = open(sys.argv[1])
@@ -15,6 +22,7 @@ def main():
 
     # build dictionary for sent_file
     scores = {} # initialize an empty dictionary
+    new_scores = {}
     afinnfile = open(sys.argv[1])
     for line in afinnfile:
     	term, score = line.split('\t')
@@ -26,7 +34,7 @@ def main():
     for line in tweet_file:
         encoded = json.loads(line)
         if encoded.get(u'text'):
-            print '#', n
+            # print '#', n
             # print encoded[u'text']
             # score
             words = encoded[u'text'].split();
@@ -34,6 +42,7 @@ def main():
             prev = -1 # nearest word in scores to the left
             for i in range(len(words)):
                 if not scores.get(words[i]):
+                    new_word = words[i];
                     # find the nearest 2 words in afinnfile
                     if next < i and next > -1:
                         prev = next
@@ -45,17 +54,21 @@ def main():
                             break
                             # next points to nearest word in score
                     if next < len(words) and prev > -1: 
-                        derived_score = (scores.get(words[next])+scores.get(words[prev]))/float(2)
+                        new_scores[new_word] = new_scores.get(new_word,0) + (scores.get(words[next])+scores.get(words[prev]))/float(2)
                     elif next < len(words) and prev == -1:
-                        derived_score = scores.get(words[next])
-                    else:
-                        derived_score = 0 # not found
-                    print 'word derived: ', words[i], derived_score
-                    if prev > -1 and prev < len(words):
-                        print 'left neighbour: ', words[prev], scores.get(words[prev])
-                    if next > -1 and next < len(words):
-                        print 'right neighbour: ', words[next], scores.get(words[next])
+                        new_scores[new_word] = new_scores.get(new_word,0) + scores.get(words[next])
+                    # else:
+                    #     derived_score = 0 # not found
+                    # print 'Derived term:'
+                    # print  uni2utf8(words[i]), derived_score
+                    # if prev > -1 and prev < len(words):
+                    #     print 'left neighbour: ', words[prev], scores.get(words[prev])
+                    # if next > -1 and next < len(words):
+                    #     print 'right neighbour: ', words[next], scores.get(words[next])
             n += 1
+    # print the new scores
+    for word in new_scores:
+        print uni2utf8(word), float(new_scores[word])
 
 
         
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,8 +1,6 @@
 import sys
 import json
 
-def hw():
-    print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -10,9 +8,8 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    # lines(sent_file)
+    # lines(tweet_file)
 
     # build dictionary for sent_file
     scores = {} # initialize an empty dictionary
