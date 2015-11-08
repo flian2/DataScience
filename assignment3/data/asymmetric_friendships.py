58145f7b76f92dbef931dd7aa1dd3d54dd29e28c
Feier Lian <flian2@ncsu.edu>
2015-11-07 23:15:40 -0500

    clean

--- assignment3/data/asymmetric_friendships.py
+++ assignment3/data/asymmetric_friendships.py
@@ -9,32 +9,30 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 def mapper(record):
-    # key: person
-    # value: his friend
-    # sorted_record = sorted(record)
-    # key = sorted_record[0]
-    # mr.emit_intermediate(key, sorted_record)
-    mr.emit(record[0],record[1])
+    # key: first one in the sorted tuple (person, friend)
+    # value: the person
+    sorted_record = sorted(record)
+    key = sorted_record[0]
+    mr.emit_intermediate(key, record)
+    
     
 
 def reducer(key, list_of_values):
+    # if there's tuple and flipped tuple in the list: symmetric
+    person = []
+    # print key
+    # print list_of_values
     
-    dic = {}
-    for record in list_of_values:
-    	tup = (record[0],record[1])
-    	if not (tup in dic):
-    		# asymetric
-    		dic[tup] = 0 
-    	else:
-    		# symmetric
-    		dic[tup] = 1
+    for record1 in list_of_values:
+        symm = 0
+        for record2 in list_of_values:
+            if record1[0] == record2[1] and record1[1] == record2[0]:
+                symm = 1
+        if symm == 0:
+            mr.emit((record1[1],record1[0]))
+            
 
-    for tup in dic:
-    	if dic[tup] == 0:
-          mr.emit((record[0],record[1]))
-    	  mr.emit((record[1],record[0]))
 
-    
 
     
 
